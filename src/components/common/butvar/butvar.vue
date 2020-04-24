0d23fb3893cfc8078cb796e171e11d7dc0516d7e
bug-hou <2510596084@qq.com>
2020-04-24 21:26:21 +0800

    first

---
+++ src/components/common/butvar/butvar.vue
@@ -0,0 +1,33 @@
+<!--  -->
+<template>
+  <div id="but">
+      <div v-for="(item,index) in data" :key="index"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+  props:{
+    data:{
+        type:Array,
+        default(){
+            return []
+        }
+    }
+  },
+  components: {},
+
+  computed: {},
+
+  mounted(){},
+
+  methods: {}
+}
+
+</script>
+<style scoped>
+</style>
